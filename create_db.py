2f7585ab0dc3ea196a5bd9168bc0d42373fcd9f7
Kivi Vuorilehto <kivi.vuorilehto@gmail.com>
2025-10-31 20:05:18 +0200

    added index creation to init.sql

--- create_db.py
+++ create_db.py
@@ -73,20 +73,21 @@ def init_database():
 def create_indexes():
     conn = get_connection()
     cursor = conn.cursor()
+    cursor.execute("CREATE INDEX IF NOT EXISTS idx_listings_status ON Listings(status);")
 
     cursor.execute("""CREATE INDEX IF NOT EXISTS idx_listings_user_status_time
-                   ON Listings (user_id, status, time_stamp DESC)""")
+                   ON Listings (user_id, status, time_stamp DESC);""")
     cursor.execute("""CREATE INDEX IF NOT EXISTS idx_listings_category_status_time
-                   ON Listings (category, status, time_stamp DESC)""")
-    cursor.execute("CREATE INDEX IF NOT EXISTS idx_listings_price ON Listings (price)")
+                   ON Listings (category, status, time_stamp DESC);""")
+    cursor.execute("CREATE INDEX IF NOT EXISTS idx_listings_price ON Listings (price);")
 
     cursor.execute("""CREATE INDEX IF NOT EXISTS idx_listing_images_listing
-                   ON ListingImages (listing_id)""")
+                   ON ListingImages (listing_id);""")
 
     cursor.execute("""CREATE INDEX IF NOT EXISTS idx_comments_listing_time
-                   ON Comments (listing_id, time_stamp ASC)""")
+                   ON Comments (listing_id, time_stamp ASC);""")
     cursor.execute("""CREATE INDEX IF NOT EXISTS idx_comments_sender
-                   ON Comments (sender_id)""")
+                   ON Comments (sender_id);""")
 
     conn.commit()
     conn.close()
--- init.sql
+++ init.sql
@@ -10,3 +10,15 @@ INSERT OR IGNORE INTO ListingCategories (category_name) VALUES
         ('Sports Equipment'),
         ('Collectibles'),
         ('Other');
+
+
+CREATE INDEX IF NOT EXISTS idx_listings_status ON Listings(status);
+
+CREATE INDEX IF NOT EXISTS idx_listings_user_status_time ON Listings (user_id, status, time_stamp DESC);
+CREATE INDEX IF NOT EXISTS idx_listings_category_status_time ON Listings (category, status, time_stamp DESC);
+CREATE INDEX IF NOT EXISTS idx_listings_price ON Listings (price);
+
+CREATE INDEX IF NOT EXISTS idx_listing_images_listing ON ListingImages (listing_id);
+
+CREATE INDEX IF NOT EXISTS idx_comments_listing_time ON Comments (listing_id, time_stamp ASC);
+CREATE INDEX IF NOT EXISTS idx_comments_sender ON Comments (sender_id);
